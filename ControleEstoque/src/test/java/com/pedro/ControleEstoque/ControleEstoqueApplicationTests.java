9ab1735ea1417faab4c1dfab4dc0860fa21c36d2
pedro <pedrofrs008@gmail.com>
2024-01-03 17:36:02 -0300

    Iniciando Diretorios do Projeto

---
+++ ControleEstoque/src/test/java/com/pedro/ControleEstoque/ControleEstoqueApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pedro.ControleEstoque;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ControleEstoqueApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
